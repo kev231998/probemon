f354b46f6827e283da8b50f38755a7005ceeeb95
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2019-10-12 16:24:06 +0200

    Cosmetic changes related to protobuf stuff

--- src/www/update-protobuf.sh
+++ src/www/update-protobuf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # helper script to update all things related to protobuf proto
-protoc=~/prog/dvcs/protobuf/src/protoc
 
-$protoc --python_out=. probe.proto
-$protoc --js_out=import_style=commonjs,binary:static/js probe.proto
+protoc --python_out=. probe.proto
+protoc --js_out=import_style=commonjs,binary:static/js probe.proto
 
 cd static/js
 if [[ ! -d node_modules ]]; then
